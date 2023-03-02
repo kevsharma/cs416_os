0ba9c2a4d8c4bd093136d71a5b1e9c9b7156c2c1
Yash <58752969+imyash01@users.noreply.github.com>
2023-03-01 22:34:41 -0500

    Creates struct and starts worker_create
    Adds recommended variables in the struct.
    Might need more changes in future based on what is needed.
    Lines in worker_create uses the struct created to allocate memory / initialize variables.
    STACK_SIZE constant is arbitrary need to look into it.

--- p2/code/thread-worker.c
+++ p2/code/thread-worker.c
@@ -6,6 +6,8 @@
 
 #include "thread-worker.h"
 
+#define STACK_SIZE 30000
+
 //Global counter for total context switches and 
 //average turn around and response time
 long tot_cntx_switches=0;
@@ -14,7 +16,7 @@ double avg_resp_time=0;
 
 
 // INITAILIZE ALL YOUR OTHER VARIABLES HERE
-// YOUR CODE HERE
+uint id_value = 0;
 
 
 /* create a new thread */
@@ -27,7 +29,15 @@ int worker_create(worker_t * thread, pthread_attr_t * attr,
        // after everything is set, push this thread into run queue and 
        // - make it ready for the execution.
 
-       // YOUR CODE HERE
+       tcb *t_tcb = malloc(sizeof(tcb));
+	   t_tcb->tid = ++id_value; //adds 1 to the value then assigns it to tid
+	   t_tcb->t_status = Ready; 
+	   t_tcb->stack = malloc(STACK_SIZE);
+	   
+	   t_tcb->t_context.uc_link=NULL;
+	   t_tcb->t_context.uc_stack.ss_sp=t_tcb->stack;
+	   t_tcb->t_context.uc_stack.ss_size=STACK_SIZE;
+	   t_tcb->t_context.uc_stack.ss_flags=0;
 	
     return 0;
 };
