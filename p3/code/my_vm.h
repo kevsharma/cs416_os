8009985bca7b6b1510bd3ad75ceff942b1635714
Kev Sharma <skevkets@gmail.com>
2023-04-02 18:45:19 -0400

    Auxiliary types for vm setup

--- p3/code/my_vm.h
+++ p3/code/my_vm.h
@@ -3,6 +3,8 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <assert.h>
+#include<string.h>
 
 //Assume the address space is 32 bits, so the max memory size is 4GB
 //Page size is 4KB
@@ -14,7 +16,7 @@
 // Maximum size of virtual memory
 #define MAX_MEMSIZE 4ULL*1024*1024*1024
 
-// Size of "physcial memory"
+// Size of "physical memory"
 #define MEMSIZE 1024*1024*1024
 
 // Represents a page table entry
@@ -47,5 +49,27 @@ int put_value(void *va, void *val, int size);
 void get_value(void *va, void *val, int size);
 void mat_mult(void *mat1, void *mat2, int size, void *answer);
 void print_TLB_missrate();
+
+/* The number of bits needed to encode some data. */
+typedef short num_bits_t;
+
+typedef struct {
+    num_bits_t va_space;            // log_2 (MAX_MEMSIZE)
+    num_bits_t pa_space;            // log_2 (MEMSIZE)
+    
+    num_bits_t offset;              // log_2 (PGSIZE)
+    num_bits_t max_physical_pages;  // pa_space - offset
+
+    num_bits_t page_table;          // log_2 (PGSIZE / sizeof(pte_t))
+    num_bits_t page_dir;            // max_pages - page_table
+
+    num_bits_t chars_for_frame_bitmap;    // max_pages - 3 {2^3 per char}
+} paging_scheme_t;
+
+num_bits_t num_bits_needed_to_encode(unsigned long val);
+void init_paging_scheme(paging_scheme_t *ps);
+void print_paging_scheme(paging_scheme_t *ps);
+
+void clean_my_vm(void);
 
 #endif
